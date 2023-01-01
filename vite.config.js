3b86234145a611312934619c83488258514bdbed
Pavel Nedrigailov <pavel.nedr@gmail.com>
2023-01-01 14:47:06 +0100

    Update to svelteKit 1.0.0-next.405

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [
+		sveltekit(),
+		basicSsl(),
+	]
+}
+
+export default config
